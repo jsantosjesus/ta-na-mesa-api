fa3ad6bc7d977decb99e31f17de7daa47488c7b4
jadson santos <jadsoonbol@gmail.com>
2024-05-06 22:27:07 -0300

    enviando mensagem para o firebase

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 import { server } from "./server/Server";
-import { initializeApp } from 'firebase-admin/app';
-
-// const app = initializeApp();
 
 
 server.listen(process.env.PORT, () => console.log('Rodando'));
