0dcd23b69dab3d1e60636b09df78f55bdacdf45e
jadson santos <jadsoonbol@gmail.com>
2024-07-02 15:27:18 -0300

    cors

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -13,14 +13,7 @@ server.use((req, res, next) => {
     res.header("methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
     res.header("Access-Control-Allow-Headers", 'Content-Type');
 
-    const corsOptions = {
-        origin: 'https://ta-na-mesa-cliente.vercel.app', // Seu domínio Vercel
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos
-        allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
-        credentials: true // Permitir cookies
-      };
-
-    server.use(cors(corsOptions));
+    server.use(cors());
 
     next();
 });
